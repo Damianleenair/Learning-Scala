18c20a7eabb697319fc372e9a122363503628244
Damian Nair <damian.nair@gmail.com>
2025-04-23 16:55:13 +0200

    Added Operators and Booleans

---
+++ src/main/scala/OperatorsAndBooleans/Operators.scala
@@ -0,0 +1,20 @@
+package OperatorsAndBooleans
+@main
+
+def operators(): Unit = {
+  //Addition
+  println(5+2)
+  println("Damian " + "Nair")
+
+  //Subtraction
+  println(8-2)
+
+  //Multiplication
+  println(7*3)
+
+  //Division
+  println(5d/2) //Double divided by an integer
+
+  //Modulus (Remainder)
+  println(5%2)
+}
